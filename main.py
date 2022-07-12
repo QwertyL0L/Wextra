3f68bc9257f6d040e24040e8a64892bdeb781e78
QwertyL0L <109008649+QwertyL0L@users.noreply.github.com>
2022-07-11 20:27:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -336,9 +336,9 @@ async def server(ctx):
 
     await ctx.send(embed=embed)
 
-    @bot.command(name="summon")  # troll command
+@bot.command(name="summon")  # troll command
 async def p(ctx):
-    await ctx.send(f"Fuck off {ctx.message.author.mention}! Im sleeping, Bitch!")
+await ctx.send(f"Fuck off {ctx.message.author.mention}! Im sleeping, Bitch!")
 
     
 bot.run(os.getenv("DISCORD_TOKEN"))
